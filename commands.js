d98a9d32508ffe81e9afc027fd0f6fed79a57bf8
JENO JIJI <jenojiji001@gmail.com>
2022-05-15 12:53:20 +0530

    'inquirer' module is used for adding nnew customer

--- commands.js
+++ commands.js
@@ -1,21 +1,54 @@
 const program = require('commander');
+const { prompt } = require('inquirer');
 const {
     addCustomer,
     findCustomer
 } = require('./index');
 
+//customer questions
+const questions = [
+    {
+        type: 'input',
+        name: 'firstname',
+        message: 'Customer First name'
+    },
+    {
+        type: 'input',
+        name: 'lastname',
+        message: 'Customer Last name'
+    },
+    {
+        type: 'input',
+        name: 'phone',
+        message: 'Customer Phone Number'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Customer Email Address'
+    }
+
+];
+
 program
     .version('1.0.0')
     .description("Client management system")
 
+// program
+//     .command('add <firstname> <lastname> <phone> <email>')
+//     .alias('a')
+//     .description('Add a customer')
+//     .action((firstname, lastname, phone, email) => {
+//         addCustomer({ firstname, lastname, phone, email });
+//     });
+
 program
-    .command('add <firstname> <lastname> <phone> <email>')
+    .command('add')
     .alias('a')
     .description('Add a customer')
-    .action((firstname, lastname, phone, email) => {
-        addCustomer({ firstname, lastname, phone, email });
+    .action(() => {
+        prompt(questions).then(answers => addCustomer(answers));
     });
-
 program
     .command('find <name>')
     .alias('f')
